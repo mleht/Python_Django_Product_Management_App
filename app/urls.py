0a197e3c6efc152f59109449c417ad4485262c58
Mikko Lehtinen <[]>
2021-04-10 17:01:02 +0300

    Suppliers and Product pages

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import landingview #eli importoidaan saman kansion views.py tiedostosta
+from .views import landingview, supplierlistview, productlistview #eli importoidaan saman kansion views.py tiedostosta
 
 urlpatterns = [
     path('', landingview),
+    path('suppliers/', supplierlistview),
+    path('products/', productlistview),
 ]
 # Sovelluksen juuressa näytetään landingview
--- app/views.py
+++ app/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .models import Supplier, Product
 
 def landingview(request):
     return render (request, "landingpage.html")
+
+def supplierlistview(request):
+    supplierlist = Supplier.objects.all() # haetaan kaikki 
+    context = {'suppliers': supplierlist}
+    return render (request, "suppliers.html", context)
+
+def productlistview(request):
+    productlist = Product.objects.all() # haetaan kaikki
+    context = {'products': productlist}
+    return render (request, "products.html", context)
