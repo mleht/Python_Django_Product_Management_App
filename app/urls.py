c5b1450aaf3fbaf81d5069790bc40fc4f9690772
Mikko Lehtinen <[]>
2021-04-10 18:38:16 +0300

    Saving supplier to database works

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import landingview, supplierlistview, productlistview #eli importoidaan saman kansion views.py tiedostosta
+from .views import landingview, supplierlistview, addsupplier, productlistview #eli importoidaan saman kansion views.py tiedostosta
 
 urlpatterns = [
     path('', landingview),
     path('suppliers/', supplierlistview),
+    path('add-supplier/', addsupplier),
     path('products/', productlistview),
 ]
 # Sovelluksen juuressa näytetään landingview
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Supplier, Product
 
 def landingview(request):
@@ -8,6 +8,20 @@ def supplierlistview(request):
     supplierlist = Supplier.objects.all() # haetaan kaikki 
     mydictionary = {'suppliers': supplierlist} # Tähän dictionaryn 'suppliers' viitataan suppliers.html for-loopissa
     return render (request, "suppliers.html", context=mydictionary)
+
+def addsupplier(request):
+    a = request.POST['companyname']
+    b = request.POST['contactname']
+    c = request.POST['address']
+    d = request.POST['phone']
+    e = request.POST['email']
+    f = request.POST['country']
+    Supplier(companyname = a, contactname = b, address = c, phone = d, email = e, country = f).save()
+    return redirect(request.META['HTTP_REFERER'])    
+    # Formilta tulee tiedot a, b jne. ja niillä luodaam modelin mukainen Supplier, joka tallennetaan heti
+    # request.POST  Only handles form data.  Only works for 'POST' method. - https://www.django-rest-framework.org/tutorial/2-requests-and-responses/
+    # HTTP_REFERER – The referring page, (if any)
+    # HttpRequest.META: A dictionary containing all available HTTP headers : https://docs.djangoproject.com/en/3.1/ref/request-response/
 
 def productlistview(request):
     productlist = Product.objects.all() # haetaan kaikki
