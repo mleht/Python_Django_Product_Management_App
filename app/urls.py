f7655227b39c729ece232bd347fed21d85f87b58
Mikko Lehtinen <[]>
2021-04-12 21:19:21 +0300

    authentication

--- app/urls.py
+++ app/urls.py
@@ -2,13 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landingview, name='landingview'),
+    path('home/', views.landingview, name='landingview'),
+
+    #login
+    path('', views.loginview, name ='loginview'),
+    path('login/', views.login_action, name='login_action'),
+    path('logout/', views.logout_action, name='logout_action'),
+
+    #suppliers
     path('suppliers/', views.supplierlistview, name='suppliers'),
     path('add-supplier/', views.addsupplier, name='add-supplier'),
     path('delete-supplier/<int:id>/', views.deletesupplier, name='delete-supplier'),
     path('edit-supplier-get/<int:id>/', views.edit_supplier_get, name='edit-supplier-get'),
     path('edit-supplier-post/<int:id>/', views.edit_supplier_post, name='edit-supplier-post'),
 
+    #products
     path('products/', views.productlistview, name='products'),
     path('add-product/', views.addproduct, name='add-product'),
     path('delete-product/<int:id>/', views.deleteproduct, name='delete-product'),
--- app/views.py
+++ app/views.py
@@ -1,23 +1,53 @@
 from django.shortcuts import render, redirect
 from .models import Supplier, Product
+from django.contrib.auth import authenticate, login, logout
 
 def landingview(request):
-    return render (request, "landingpage.html")
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        return render (request, "landingpage.html")
 
+#login & logout
+def loginview(request):
+    return render (request, "loginpage.html")
+
+def login_action(request):
+    user = request.POST['username']
+    passw = request.POST['password']
+    user = authenticate(username = user, password = passw)
+    if user:
+        login(request, user)
+        mydictionary = {'name' : user}
+        return render(request, 'landingpage.html', context=mydictionary)
+    else:
+        return render(request, 'loginerror.html')
+
+def logout_action(request):
+    logout(request)
+    return render(request, 'loginpage.html')
+
+#suppliers
 def supplierlistview(request):
-    supplierlist = Supplier.objects.all() # haetaan kaikki 
-    mydictionary = {'suppliers': supplierlist} # Tähän dictionaryn 'suppliers' viitataan suppliers.html for-loopissa
-    return render (request, "suppliers.html", context=mydictionary)
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        supplierlist = Supplier.objects.all() # haetaan kaikki 
+        mydictionary = {'suppliers': supplierlist} # Tähän dictionaryn 'suppliers' viitataan suppliers.html for-loopissa
+        return render (request, "suppliers.html", context=mydictionary)
 
 def addsupplier(request):
-    a = request.POST['companyname']
-    b = request.POST['contactname']
-    c = request.POST['address']
-    d = request.POST['phone']
-    e = request.POST['email']
-    f = request.POST['country']
-    Supplier(companyname = a, contactname = b, address = c, phone = d, email = e, country = f).save()
-    return redirect(request.META['HTTP_REFERER'])    
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        a = request.POST['companyname']
+        b = request.POST['contactname']
+        c = request.POST['address']
+        d = request.POST['phone']
+        e = request.POST['email']
+        f = request.POST['country']
+        Supplier(companyname = a, contactname = b, address = c, phone = d, email = e, country = f).save()
+        return redirect(request.META['HTTP_REFERER'])    
     # Formilta tulee tiedot a, b jne. ja niillä luodaan modelin mukainen Supplier, joka tallennetaan heti
     # request.POST  Only handles form data.  Only works for 'POST' method. - https://www.django-rest-framework.org/tutorial/2-requests-and-responses/
     # HTTP_REFERER – The referring page, (if any)
@@ -25,67 +55,94 @@ def addsupplier(request):
 
 
 def deletesupplier(request,id):
-    Supplier.objects.filter(id = id).delete() # deletoidaan se supplier, jonka id on sama kuin parametrina saatu (jälkimmäinen on parametri)
-    return redirect(request.META['HTTP_REFERER'])
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        Supplier.objects.filter(id = id).delete() # deletoidaan se supplier, jonka id on sama kuin parametrina saatu (jälkimmäinen on parametri)
+        return redirect(request.META['HTTP_REFERER'])
 
 def edit_supplier_get(request, id):
-    supplier = Supplier.objects.filter(id = id)
-    mydictionary = {'supplier': supplier}
-    return render (request,"edit_supplier.html",context=mydictionary)
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        supplier = Supplier.objects.filter(id = id)
+        mydictionary = {'supplier': supplier}
+        return render (request,"edit_supplier.html",context=mydictionary)
 
 def edit_supplier_post(request, id):
-    item = Supplier.objects.get(id = id)
-    item.companyname = request.POST['companyname']
-    item.contactname = request.POST['contactname']
-    item.address = request.POST['address']
-    item.phone = request.POST['phone']
-    item.email = request.POST['email']
-    item.country = request.POST['country']
-    item.save()
-    return redirect(supplierlistview)
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        item = Supplier.objects.get(id = id)
+        item.companyname = request.POST['companyname']
+        item.contactname = request.POST['contactname']
+        item.address = request.POST['address']
+        item.phone = request.POST['phone']
+        item.email = request.POST['email']
+        item.country = request.POST['country']
+        item.save()
+        return redirect(supplierlistview)
 
-
+#products
 def productlistview(request):
-    productlist = Product.objects.all() # haetaan kaikki
-    mydictionary = {'products': productlist} # Tähän dictionaryn 'products' viitataan products.html for-loopissa
-    return render (request, "products.html", context=mydictionary)
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        productlist = Product.objects.all() # haetaan kaikki
+        mydictionary = {'products': productlist} # Tähän dictionaryn 'products' viitataan products.html for-loopissa
+        return render (request, "products.html", context=mydictionary)
 
 def addproduct(request):
-    a = request.POST['productname']
-    b = request.POST['packagesize']
-    c = request.POST['unitprice']
-    d = request.POST['unitsinstock']
-    e = request.POST['companyname']
-    Product(productname = a, packagesize = b, unitprice = c, unitsinstock = d, companyname = e).save()
-    return redirect(request.META['HTTP_REFERER'])
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        a = request.POST['productname']
+        b = request.POST['packagesize']
+        c = request.POST['unitprice']
+        d = request.POST['unitsinstock']
+        e = request.POST['companyname']
+        Product(productname = a, packagesize = b, unitprice = c, unitsinstock = d, companyname = e).save()
+        return redirect(request.META['HTTP_REFERER'])
     # Formilta tulee tiedot a, b jne. ja niillä luodaan modelin mukainen Product, joka tallennetaan heti
     # request.POST  Only handles form data.  Only works for 'POST' method. - https://www.django-rest-framework.org/tutorial/2-requests-and-responses/
     # HTTP_REFERER – The referring page, (if any)
     # HttpRequest.META: A dictionary containing all available HTTP headers : https://docs.djangoproject.com/en/3.1/ref/request-response/
 
 def deleteproduct(request,id):
-    Product.objects.filter(id = id).delete() #deletoidaan se tuote, jonka id on sama kuin parametrina saatu id (jälkimmäinen on parametri)
-    return redirect(request.META['HTTP_REFERER'])
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        Product.objects.filter(id = id).delete() #deletoidaan se tuote, jonka id on sama kuin parametrina saatu id (jälkimmäinen on parametri)
+        return redirect(request.META['HTTP_REFERER'])
 
 def edit_product_get(request, id):
-    product = Product.objects.filter(id = id)
-    mydictionary = {'product': product}
-    return render (request,"edit_product.html", context=mydictionary)
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        product = Product.objects.filter(id = id)
+        mydictionary = {'product': product}
+        return render (request,"edit_product.html", context=mydictionary)
 
 def edit_product_post(request, id):
-    item = Product.objects.get(id = id)
-    item.productname = request.POST['productname']
-    item.packagesize = request.POST['packagesize']
-    item.unitprice = request.POST['unitprice']
-    item.unitsinstock = request.POST['unitsinstock']
-    item.companyname = request.POST['companyname']
-    item.save()
-    return redirect(productlistview)
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        item = Product.objects.get(id = id)
+        item.productname = request.POST['productname']
+        item.packagesize = request.POST['packagesize']
+        item.unitprice = request.POST['unitprice']
+        item.unitsinstock = request.POST['unitsinstock']
+        item.companyname = request.POST['companyname']
+        item.save()
+        return redirect(productlistview)
 
 def products_by_supplier(request,id):
-    supplierObj = Supplier.objects.get(id = id)
-    company = supplierObj.companyname
-    productlist = Product.objects.all()
-    filteredproducts = productlist.filter(companyname = company)
-    mydictionary = {'products':filteredproducts}
-    return render (request,"products.html", context=mydictionary)
+    if not request.user.is_authenticated:
+        return render(request, 'loginpage.html')
+    else:
+        supplierObj = Supplier.objects.get(id = id)
+        company = supplierObj.companyname
+        productlist = Product.objects.all()
+        filteredproducts = productlist.filter(companyname = company)
+        mydictionary = {'products':filteredproducts}
+        return render (request,"products.html", context=mydictionary)
